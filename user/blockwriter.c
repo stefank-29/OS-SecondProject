a3792450aead97758ca82ab929d8af9cf89d08dc
stefank-29 <stefankaraferovic@gmail.com>
2021-04-04 20:01:42 +0200

    blockwriter help

--- user/blockwriter.c
+++ user/blockwriter.c
@@ -6,26 +6,47 @@
 
 void
 blockwriter(char *name, int blockNum){
+    int fd, size, k;
 
+    fd = open(name, O_CREATE | O_RDWR); // file
+    size=512;
+
+    char niz[512] = "8arCLufFuKR5a9CDlIHvHNCvhMipyM0HD2GLX6q349NYW4IaZ9Xy67FtfRiegZu4V1yZ0hESKl22CEgItYkBmqIYJxIHonYm84fZnPloOGqIuh6gokrsOAqUuu2L3tNIv9MjIXQsfJsKfTrsRPBfzFICZnVr4egeEnQBFJdAGMXzQBQscgeQvIGowxH7EpLgBj5YsIUcMZ3byiW83Rf6HpZRs72Uv8FmwIwWaYbbGdFMHGSl5qAoFq5VPV2rB2CvAvYBkGJsLWEX8bhJSp1oJmNu0kdchO5SZLriwtNWdFVNomsAgEITwOEXqkHXgre3xJGkXCHQIAKbhPuHtIgn4KSCJSpgB3HY58mOSKIQGqWTPYhfuL1YtQ8jj9gT0GBWdkaFfSolUbDQDQLMzdsv3SHN108CAsMxBbXzEq1pzFevyUQFUuw3pIYJjpN6TyM3Oer6hukvQwncrBmDjkXa9NtW4ZtUOZEwE8PAerLEg9PeveMi3Iao7CBtE4gMxUUT";// niz od 32 * 16
+    for(int i = 0; i < blockNum; i++){
+        printf("Writing block %d\n", i);
+        if(write(fd, &niz, size) != size) {
+            printf("write error\n");
+        }
+    }
 }
 
 int
 main(int argc, char *argv[])
 {
     int fd, size, k;
+    static char* filename;
 
-    k = atoi(argv[1]); // hvatam argument u komandnoj liniji
-    k = 150;
+   // k = atoi(argv[1]); // hvatam argument u komandnoj liniji
 
-    fd = open("long.txt", O_CREATE | O_RDWR); // file
-    size=512;
-
-    char niz[512] = "8arCLufFuKR5a9CDlIHvHNCvhMipyM0HD2GLX6q349NYW4IaZ9Xy67FtfRiegZu4V1yZ0hESKl22CEgItYkBmqIYJxIHonYm84fZnPloOGqIuh6gokrsOAqUuu2L3tNIv9MjIXQsfJsKfTrsRPBfzFICZnVr4egeEnQBFJdAGMXzQBQscgeQvIGowxH7EpLgBj5YsIUcMZ3byiW83Rf6HpZRs72Uv8FmwIwWaYbbGdFMHGSl5qAoFq5VPV2rB2CvAvYBkGJsLWEX8bhJSp1oJmNu0kdchO5SZLriwtNWdFVNomsAgEITwOEXqkHXgre3xJGkXCHQIAKbhPuHtIgn4KSCJSpgB3HY58mOSKIQGqWTPYhfuL1YtQ8jj9gT0GBWdkaFfSolUbDQDQLMzdsv3SHN108CAsMxBbXzEq1pzFevyUQFUuw3pIYJjpN6TyM3Oer6hukvQwncrBmDjkXa9NtW4ZtUOZEwE8PAerLEg9PeveMi3Iao7CBtE4gMxUUT";// niz od 32 * 16
-    for(int i = 0; i < k; i++){
-        printf("Writing block %d\n", i);
-        if(write(fd, &niz, size) != size) {
-            printf("write error\n");
-        }
+    if(argc < 2){
+        blockwriter("long.txt", 150);
+        exit();
     }
+
+    if(strcmp(argv[1], "-h") || strcmp(argv[1], "--help")){
+        printf("\nUse this program to create a big file filled with a-z, A-Z and 0-9 characters.\n");
+        printf("Default filename: long.txt\n");
+        printf("Default blocks: 150\n");
+        printf("Usage: blockwriter [OPTION]...\n\n");
+
+        printf("Command line options:\n");
+        printf("         -h, --help: Show help promt.\n");
+        printf("         -b, --blocks: Number of blocks to write.\n");
+        printf("         -o, --output-file: Set output filename\n");
+
+    }
+
+
+
 	exit(); // mora exit na kraju poziva
 }
