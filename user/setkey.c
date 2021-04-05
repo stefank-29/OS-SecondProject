9c2a7b629726648073fd500a8d82a6649b9d8d06
stefank-29 <stefankaraferovic@gmail.com>
2021-04-05 15:31:10 +0200

    STDIN for secret key

--- user/setkey.c
+++ user/setkey.c
@@ -7,12 +7,35 @@
 int
 main(int argc, char *argv[])
 {
-    int fd, size, k, i, n = 150;
-    char* filename = "long.txt";
+    int fd, n, i;
+    char *number;
 
    // k = atoi(argv[1]); // hvatam argument u komandnoj liniji
 
-    setkey(5);
+     for(i = 1; i < argc; i++){
+        if(!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help")){
+            printf("\nUse this program to set the current active key.\n");
+            printf("After setting the key, you can use encr and decr with that key.\n");
+            printf("Usage: setkey [OPTION]... [KEY]\n\n");
+
+            printf("Command line options:\n");
+            printf("         -h, --help: Show help promt.\n");
+            printf("         -s, --secret: Enter the key via STDIN. Hide key when entering it.\n");
+            exit();
+        }
+        if(!strcmp(argv[i], "-s") || !strcmp(argv[i], "--secret")){
+           // iskljuciti echo
+            printf("Enter the key: ");
+            gets(number, 10);
+            n = atoi(number);
+            printf("%d", n);
+            exit();
+        }
+
+
+    }
+
+    setkey(n);
 
 	exit(); // mora exit na kraju poziva
 }
